d6bdb75797d6305a26dc3e3b17f711404397a4fc
frances <frankie.rae1966@gmail.com>
2018-05-29 20:33:13 -0400

    first commit

---
+++ module.js
@@ -0,0 +1,14 @@
+"use strict"; 
+console.log("Module here!"); 
+
+angular.module("app",["ngRoute"])
+.config(($routeProvider) => {
+  $routeProvider
+    .when("/profile", {
+      template: "<profile></profile>"
+    })
+    .when("/edit-profile", {
+      template: "<edit-profile></edit-profile>"
+    })
+    .otherwise({ redirectTo: "/add" });
+});
